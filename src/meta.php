7d572329790d9c8c665d2bb8126621bfb87289a9
Sean Fisher <srtfisher@gmail.com>
2025-08-22 15:39:01 -0400

    Adding playwright tests

--- src/meta.php
+++ src/meta.php
@@ -13,16 +13,18 @@ namespace Alley\WP\Modified_Date_Control;
 const META_KEY_ALLOW_UPDATES = 'wp_modified_date_control_allow_updates';
 
 // Register the meta for the plugin.
-register_meta( 'post', META_KEY_ALLOW_UPDATES, [
-	'type'         => 'boolean',
-	'description'  => __( 'Allow updates to the modified date.', 'wp-modified-date-control' ),
-	'single'       => true,
-	'show_in_rest' => true,
+add_action( 'init', function () {
+	register_meta( 'post', META_KEY_ALLOW_UPDATES, [
+		'type'         => 'boolean',
+		'description'  => __( 'Allow updates to the modified date.', 'wp-modified-date-control' ),
+		'single'       => true,
+		'show_in_rest' => true,
 
-	/**
-	 * Filter the default value for allowing updates to the modified date.
-	 *
-	 * @param bool $default The default value.
-	 */
-	'default'      => (bool) apply_filters( 'wp_modified_date_control_default_allow_updates', true ),
-] );
+		/**
+		 * Filter the default value for allowing updates to the modified date.
+		 *
+		 * @param bool $default The default value.
+		 */
+		'default'      => (bool) apply_filters( 'wp_modified_date_control_default_allow_updates', true ),
+	] );
+} );
