5cb32e27f73e0bab8605c266cb589db353981525
srtfisher <srtfisher@users.noreply.github.com>
2025-08-25 02:32:37 +0000

    Built changes for v1.0.1 from develop 154a50af594c50eb5aace5eb09e3ff56129ea420

---
+++ build/plugin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '7c4a0ac596c60a0cc64d');
